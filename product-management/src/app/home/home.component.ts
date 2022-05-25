538acb213b0c4936d8240c909968c6d3b2bfcf91
Jak Ashuraliev <ashuraliev@gmail.com>
2022-05-24 17:00:06 -0700

    Add product images, create products object and render on the frontend

--- product-management/src/app/home/home.component.ts
+++ product-management/src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public pageTitle = "Home Page";
+
   constructor() { }
 
   ngOnInit(): void {
